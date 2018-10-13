40f0474d5534d95fb21e70d9ec3efacdb4bb63be
jhl276@ucsd.edu <unconfigured@null.spigotmc.org>
2018-10-13 13:05:45 -0700

    Added Description to Home screen

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -8,17 +8,28 @@ class HomeScreen extends Component {
         return (
             <div className='homeScreen' > 
                 <div className='row' >
-                    <div className='col-1-3' ></div>
+                    <div className='col-1-3 float-right bottom' >
+                        <h1 className='title white'>VERIFY</h1></div>
                     <div className='col-1-3' ></div>
                     <div className='col-1-3 float-right bottom' >
-                        <h1>GET</h1>
+                        <h1 className='subtitle'>GET</h1>
                     </div>
                 </div>
                 <div className='row' >
-                    <div className='col-1-3' ></div>
+                    <div className='col-1-3 float-right top' >
+                        <h2 className='desc white'>Bringing Cloud Solutions and Facial Recognition 
+                            to the classroom to 
+                            enhance the learning experience.
+                        </h2>
+                        <Button 
+                            className="btn about"
+                        >
+                            Learn More<Icon type="right" />
+                        </Button>
+                    </div>
                     <div className='col-1-3' ></div>
                     <div className='col-1-3 float-right top' >
-                        <h1>STARTED</h1>
+                        <h1 className='subtitle'>STARTED</h1>
                     </div>
                 </div>
                 <div className='row' >
@@ -26,13 +37,13 @@ class HomeScreen extends Component {
                     <div className='col-1-3' ></div>
                     <div className='col-1-3 float-right center' >
                         <Button 
-                            type="primary"
+                            // type="primary"
                             className="btn signin"
                         >
                             <Icon type="login" />Login
                         </Button>
                         <Button 
-                            type="primary"
+                            // type="primary"
                             className="btn register"
                         >
                             <Icon type="form" />Register
