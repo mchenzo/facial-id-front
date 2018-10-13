52bd27fa67a1cbbe3395bb408b9fc4adc506a98d
Michael Chen <michael.chen880@gmail.com>
2018-10-13 13:26:14 -0700

    added verification modal

---
+++ src/components/VerificationModal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class VerificationModal extends Component {
+    render() {
+        return (
+            <div>Modal</div>
+        )
+    }
+}
+
+export default VerificationModal
